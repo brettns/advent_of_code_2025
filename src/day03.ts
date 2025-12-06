6a55e9e59adda64ea29cb5d894a37dc78ef31c91
bns <brettnstewart@gmail.com>
2025-12-06 13:54:40 -0500

    cleanup

--- src/day03.ts
+++ src/day03.ts
@@ -3,7 +3,7 @@ const lines = (await Bun.file('input/day03').text()).split('\n')
 const num_batteries = 12
 let total = 0
 
-for await (const line of lines) {
+for (const line of lines) {
     let idx = 0
     let joltage = 0
 
