680158e11a23e5b5ab126ebacc2cb30a3454856b
bns <brettnstewart@gmail.com>
2025-12-06 21:44:03 -0500

    removed redudant variable

--- src/day01.ts
+++ src/day01.ts
@@ -5,15 +5,13 @@ let dial = 50
 for (const line of lines) {
     const dir = line[0] === 'L' ? -1 : 1
     let amount = parseInt(line.substring(1))
-    let num_zeroes = 0
 
     if (dir > 0 || dial === 0) {
-        num_zeroes = Math.floor((dial + amount) / 100)
+        total_zeroes += Math.floor((dial + amount) / 100)
     } else {
-        num_zeroes = Math.floor((100 - dial + amount) / 100)
+        total_zeroes += Math.floor((100 - dial + amount) / 100)
     }
 
-    total_zeroes += num_zeroes
     const dist = dir * (amount % 100)
 
     // javascript can't do mod on negatives
